d0de9163ca0bfd9d239e3877495540df8e6643b3
gywns0417 <gywns0417@gmail.com>
2026-07-30 01:21:36 +0900

    chore: 스프링 부트 프로젝트 초기 설정 파일 생성

---
+++ src/main/java/ject/official_qr_checkin_server/OfficialQrCheckinServerApplication.java
@@ -0,0 +1,13 @@
+package ject.official_qr_checkin_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficialQrCheckinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficialQrCheckinServerApplication.class, args);
+	}
+
+}
